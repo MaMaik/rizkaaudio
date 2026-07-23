4838c3929a1327f7a09f781cc7df88e691a9e0e3
MaMaik <farrel0821@gmail.com>
2026-07-23 19:04:11 +0700

    Update

--- src/data/config.js
+++ src/data/config.js
@@ -4,10 +4,10 @@ export const toko = {
   tagline: 'Menjual Barang Audio Khususnya Audio Mobil',
   // TODO: replace with the real WhatsApp number in international format, no "+" or "0".
   // Example for an Indonesian number 0812-3456-7890 -> "6281234567890"
-  whatsapp: '6281234567890',
+  whatsapp: '6283802301230',
   alamat: 'Padang Pasir IV No.28A, Padang Pasir, Kec. Padang Barat, Kota Padang, Sumatera Barat 25115',
   // TODO: fill in real operating hours
-  jam: 'Buka setiap hari · 09.00 – 21.00 WIB',
+  jam: 'Buka setiap hari · 07.00 – 23.00 WIB',
   maps: 'https://maps.google.com/?q=Padang+Pasir+IV+No.28A+Padang+Barat+Padang+Sumatera+Barat',
   instagram: 'https://instagram.com/rizkaaudio_padang',
   facebook: 'https://www.facebook.com/share/17ukY5S5d8/',
